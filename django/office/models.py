5a1889ad8cafdf18fd21591dfa42f98197da85cf
Pg Biel <9021226+PgBiel@users.noreply.github.com>
2022-10-30 01:16:13 -0300

    django: Ajustar modelos à interface Pontuacao

--- django/office/models.py
+++ django/office/models.py
@@ -8,6 +8,9 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     genero = models.CharField(max_length=1)
     endereco = models.CharField(max_length=200)
+    pontuacao = models.FloatField(default=0.0)
+    max_pontos = models.FloatField(default=100.0)
+    min_pontos = models.FloatField(default=0.0)
 
     def __str__(self):
         return f"Nome: {self.nome}; Gênero: {self.genero}; Endereço: {self.endereco} \n"
@@ -20,6 +23,9 @@ class Carona(models.Model):
     genero = models.CharField(max_length=1)
     vagas = models.IntegerField(default=0)
     preco = models.FloatField("preço", default=0.)
+    pontuacao = models.FloatField(default=0.0)
+    max_pontos = models.FloatField(default=100.0)
+    min_pontos = models.FloatField(default=0.0)
 
     def __str__(self):
         return f"Destino: {self.destino}. Início {self.inicio}. Data e hora: {self.data_hora}. Motorista: {self.motorista}. Gênero: {self.genero} Vagas: {self.vagas} Preço: {self.preco} \n"
