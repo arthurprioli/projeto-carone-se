e56baca7f8c961ef2cea8b0991e63d45058826c0
Pg Biel <9021226+PgBiel@users.noreply.github.com>
2022-11-29 00:54:37 -0300

    django: conectar User a Usuario; começar testes de login

--- django/office/admin.py
+++ django/office/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import Carona, Usuario
+
+class UsuarioInline(admin.StackedInline):
+    model = Usuario
+    can_delete = False
+    verbose_name_plural = 'usuario'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UsuarioInline,)
 
 class CaronaAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
@@ -8,5 +17,7 @@ class CaronaAdmin(admin.ModelAdmin):
 class UsuarioAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Carona, CaronaAdmin)
 admin.site.register(Usuario, UsuarioAdmin)
--- django/office/models.py
+++ django/office/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.forms import CharField, DateField, DateTimeField, FloatField, IntegerField
 
 # Create your models here.
-
     
 class Usuario(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)  # null apenas por compatibilidade (NÃO DEVE ACONTECER NA PRÁTICA)
     nome = models.CharField(max_length=100)
     genero = models.CharField(max_length=1)
     endereco = models.CharField(max_length=200)
+    carona = models.ForeignKey("Carona", null=True, blank=True, on_delete=models.SET_NULL)  # pode não ter uma carona no momento.
     pontuacao = models.FloatField(default=0.0)
     max_pontos = models.FloatField(default=100.0)
     min_pontos = models.FloatField(default=0.0)
@@ -20,7 +23,6 @@ class Carona(models.Model):
     inicio =   models.CharField(max_length=100)
     data_hora = models.DateTimeField("data/hora")
     motorista = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="%(app_label)s_%(class)s_motorista")
-    passageiros = models.ManyToManyField(Usuario, related_name="%(app_label)s_%(class)s_passageiros", blank=True)
     vagas = models.IntegerField(default=0)
     preco = models.FloatField("preço", default=0.)
     pontuacao = models.FloatField(default=0.0)
